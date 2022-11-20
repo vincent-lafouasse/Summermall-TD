38f83cfdcc2787aaf82539773b933e9c0dbff882
Tristan Stérin <tristan.sterin@mu.ie>
2022-11-20 20:22:04 +0100

    Better minimal example

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,44 @@
 #define SCREEN_HEIGHT 480
 
 int main(int argc, char *args[]) {
-  SDL_Window *window = NULL;
-  SDL_Surface *screenSurface = NULL;
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
-    return 1;
+
+  SDL_Window *window = SDL_CreateWindow(
+      "Summermall TD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+      SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
+
+  if (window == nullptr) {
+    SDL_Log("Could not create a window: %s", SDL_GetError());
+    return -1;
   }
-  window = SDL_CreateWindow("Summermall TD", SDL_WINDOWPOS_UNDEFINED,
-                            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                            SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-  if (window == NULL) {
-    fprintf(stderr, "could not create window: %s\n", SDL_GetError());
-    return 1;
+
+  SDL_Renderer *renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (renderer == nullptr) {
+    SDL_Log("Could not create a renderer: %s", SDL_GetError());
+    return -1;
   }
-  screenSurface = SDL_GetWindowSurface(window);
-  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 0, 0));
-  SDL_UpdateWindowSurface(window);
-  SDL_Delay(2000);
+
+  while (true) {
+    // Get the next event
+    SDL_Event event;
+    if (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        // Break out of the loop on quit
+        break;
+      }
+    }
+
+    // Set the color to cornflower blue and clear
+    SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
+    SDL_RenderClear(renderer);
+    // Show the renderer contents
+    SDL_RenderPresent(renderer);
+  }
+
+  // Tidy up
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
+
   return 0;
 }
