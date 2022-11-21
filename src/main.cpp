a41a9b9b8221f637de0f7909a62e004fec5e0c94
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2022-11-21 15:26:14 +0100

    Implemented FPS computation

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,12 @@
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
+#define FPS_INTERVAL_IN_MS 1000.0  // milliseconds
 
 int main(int argc, char* args[]) {
+  Uint32 fps_last_time_fps_was_computed = SDL_GetTicks();
+  Uint32 fps_current_fps;
+  Uint32 fps_elapsed_frames_since_last_time = 0;
   SDL_Window* window = SDL_CreateWindow(
       "Summermall TD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
       SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
@@ -30,6 +34,17 @@ int main(int argc, char* args[]) {
         break;
       }
     }
+
+    fps_elapsed_frames_since_last_time ++;
+    if (SDL_GetTicks() > fps_last_time_fps_was_computed + FPS_INTERVAL_IN_MS) {
+      // If more than FPS_INTERVAL_IN_MS has elapsed since
+      // fps_last_time_fps_was_computed then compute estimated fps and reset
+      // fps_last_time[...] and fps_elapsed_frames[...]
+      fps_last_time_fps_was_computed = SDL_GetTicks();
+      fps_current_fps = fps_elapsed_frames_since_last_time;
+      fps_elapsed_frames_since_last_time = 0;
+    }
+    printf("%i\n", fps_current_fps);
 
     // Set the color to cornflower blue and clear
     SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
