53f6d1b74d8a6ba794fb77cdfb37a3f1c9d2ed3e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2022-12-14 20:50:49 +0100

    test tilewidth

--- test/src/test_map.cpp
+++ test/src/test_map.cpp
@@ -5,4 +5,7 @@ const char* BASIC_1P_MAP_PATH = "assets/maps/basic_1P.tmx";
 
 TEST(Map, Metadata) {
   Map map = make_map_from_tmx(BASIC_1P_MAP_PATH);
+  unsigned int expected_tilewidth = 32;
+
+  ASSERT_EQ(expected_tilewidth, map.tilewidth);
 }
