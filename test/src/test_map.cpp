485d9f77eab601c93c290a77baeb57b3a932f418
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2022-12-17 21:07:40 +0100

    complete testing of metadata of basic 1P map

--- test/src/test_map.cpp
+++ test/src/test_map.cpp
@@ -3,9 +3,15 @@
 
 const char* BASIC_1P_MAP_PATH = "assets/maps/basic_1P.tmx";
 
-TEST(Map, Metadata) {
+TEST(Basic1PMap, Metadata) {
   Map map = make_map_from_tmx(BASIC_1P_MAP_PATH);
   unsigned int expected_tilewidth = 32;
+  unsigned int expected_tileheight = 32;
+  unsigned int expected_width = 25;
+  unsigned int expected_height = 45;
 
   ASSERT_EQ(expected_tilewidth, map.tilewidth);
+  ASSERT_EQ(expected_tileheight, map.tileheight);
+  ASSERT_EQ(expected_width, map.width);
+  ASSERT_EQ(expected_height, map.height);
 }
