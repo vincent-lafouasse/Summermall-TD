e5349d36960c18dda0c4b107cb3e00edbe808cf8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2022-12-24 23:26:05 +0100

    remove internal functions from header

--- src/map.cpp
+++ src/map.cpp
@@ -32,6 +32,38 @@ void render_map(const Map map,
   }
 }
 
+int_vector_1D vector_1D_from_string_line(char* line_string) {
+  int_vector_1D output;
+  const char* cell_delim = ",";
+  char* cell = NULL;
+  char* mem;
+  output.clear();
+
+  cell = strtok_r(line_string, cell_delim, &mem);
+  while (cell != NULL) {
+    output.push_back(atoi(cell));
+    cell = strtok_r(NULL, cell_delim, &mem);
+  }
+  return output;
+}
+
+int_vector_2D vector_2D_from_string_csv(char* csv_string) {
+  int_vector_2D output;
+  const char* line_delim = "\n";
+  char* line = NULL;
+  char* mem;
+  output.clear();
+
+  line = strtok_r(csv_string, line_delim, &mem);
+  while (line != NULL) {
+    int_vector_1D res = vector_1D_from_string_line(line);
+    output.push_back(res);
+    line = strtok_r(NULL, line_delim, &mem);
+  }
+
+  return output;
+}
+
 const Map parse_map_from_tmx(const char* tmx_path) {
   using namespace tinyxml2;
   XMLDocument map_xml;
@@ -61,38 +93,6 @@ const Map parse_map_from_tmx(const char* tmx_path) {
   }
   Map map = {map_shape, map_src_tileshape, layers};
   return map;
-}
-
-int_vector_2D vector_2D_from_string_csv(char* csv_string) {
-  int_vector_2D output;
-  const char* line_delim = "\n";
-  char* line = NULL;
-  char* mem;
-  output.clear();
-
-  line = strtok_r(csv_string, line_delim, &mem);
-  while (line != NULL) {
-    int_vector_1D res = vector_1D_from_string_line(line);
-    output.push_back(res);
-    line = strtok_r(NULL, line_delim, &mem);
-  }
-
-  return output;
-}
-
-int_vector_1D vector_1D_from_string_line(char* line_string) {
-  int_vector_1D output;
-  const char* cell_delim = ",";
-  char* cell = NULL;
-  char* mem;
-  output.clear();
-
-  cell = strtok_r(line_string, cell_delim, &mem);
-  while (cell != NULL) {
-    output.push_back(atoi(cell));
-    cell = strtok_r(NULL, cell_delim, &mem);
-  }
-  return output;
 }
 
 void print_1D_int_vector(int_vector_1D const input) {
--- src/map.h
+++ src/map.h
@@ -30,7 +30,5 @@ void render_map(const Map map,
                 const Rectangle dst_tileshape,
                 SDL_Renderer* renderer);
 const Map parse_map_from_tmx(const char* tmx_path);
-int_vector_2D vector_2D_from_string_csv(char* csv_string);
-int_vector_1D vector_1D_from_string_line(char* line_string);
 void print_1D_int_vector(const int_vector_1D input);
 void print_2D_int_vector(const int_vector_2D input);
