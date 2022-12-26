3bbf178aaac724f322e15b5074f9a1fc88e84774
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2022-12-26 14:37:41 +0100

    minor refactor

--- src/map.cpp
+++ src/map.cpp
@@ -1,36 +1,7 @@
 #include "map.h"
 
-static int_vector_1D vector_1D_from_string_line(char* line_string) {
-  int_vector_1D output;
-  const char* cell_delim = ",";
-  char* cell = NULL;
-  char* mem;
-  output.clear();
-
-  cell = strtok_r(line_string, cell_delim, &mem);
-  while (cell != NULL) {
-    output.push_back(atoi(cell));
-    cell = strtok_r(NULL, cell_delim, &mem);
-  }
-  return output;
-}
-
-static int_vector_2D vector_2D_from_string_csv(char* csv_string) {
-  int_vector_2D output;
-  const char* line_delim = "\n";
-  char* line = NULL;
-  char* mem;
-  output.clear();
-
-  line = strtok_r(csv_string, line_delim, &mem);
-  while (line != NULL) {
-    int_vector_1D res = vector_1D_from_string_line(line);
-    output.push_back(res);
-    line = strtok_r(NULL, line_delim, &mem);
-  }
-
-  return output;
-}
+static int_vector_1D vector_1D_from_string_line(char* line_string);
+static int_vector_2D vector_2D_from_string_csv(char* csv_string);
 
 const Map parse_map_from_tmx(const char* tmx_path) {
   using namespace tinyxml2;
@@ -54,11 +25,40 @@ const Map parse_map_from_tmx(const char* tmx_path) {
   layers.clear();
   while (layer) {
     layer_csv = (char*)layer->FirstChildElement("data")->GetText();
-
     layers.push_back(vector_2D_from_string_csv(layer_csv));
-
     layer = layer->NextSiblingElement();
   }
   Map map = {map_shape, map_src_tileshape, layers};
   return map;
+}
+
+static int_vector_2D vector_2D_from_string_csv(char* csv_string) {
+  const char* line_delim = "\n";
+  char* line;
+  char* mem;
+  int_vector_2D output;
+  output.clear();
+
+  line = strtok_r(csv_string, line_delim, &mem);
+  while (line != NULL) {
+    int_vector_1D res = vector_1D_from_string_line(line);
+    output.push_back(res);
+    line = strtok_r(NULL, line_delim, &mem);
+  }
+  return output;
+}
+
+static int_vector_1D vector_1D_from_string_line(char* line_string) {
+  const char* cell_delim = ",";
+  char* cell;
+  char* mem;
+  int_vector_1D output;
+  output.clear();
+
+  cell = strtok_r(line_string, cell_delim, &mem);
+  while (cell != NULL) {
+    output.push_back(atoi(cell));
+    cell = strtok_r(NULL, cell_delim, &mem);
+  }
+  return output;
 }
