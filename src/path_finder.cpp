773c8f2f361a2b27cdc58c995096dab9ae96cbf2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-10 17:02:51 +0100

    setup for BFS path-finding

--- src/path_finder.cpp
+++ src/path_finder.cpp
@@ -1,1 +1,7 @@
-std::queue<Position> queue;
+#include "path_finder.h"
+
+void BFS(Position entrance_pos, Position exit_pos, Map* map) {
+  Node entrance = {entrance_pos, NULL, NULL};
+  Queue q;
+  std::vector<Node> visited;
+}
--- src/path_finder.h
+++ src/path_finder.h
@@ -1,2 +1,17 @@
+#pragma once
+
 #include <queue>
+#include <vector>
 #include "geometry.h"
+#include "map.h"
+
+struct Node {
+  Position position;
+  Node* previous;
+  Node* next;
+};
+
+typedef std::queue<Node> Queue;
+
+void BFS(Position entrance_pos, Position exit_pos, Map* map);
+void add_neighbours_to_queue(Node node, Map* map, Queue queue);
