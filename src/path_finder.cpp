f58e14d94a607d07aa18e1637b9eb76663e0efa4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-10 21:28:35 +0100

    start bfs again

--- src/path_finder.cpp
+++ src/path_finder.cpp
@@ -1,19 +1,1 @@
 #include "path_finder.h"
-
-void BFS(Position entrance_pos, Position exit_pos, Map* map) {
-  Node entrance = {entrance_pos, NULL, NULL};
-  Queue queue;
-  std::vector<Node> reached;
-  queue.push(entrance);
-
-  while (!queue.empty()) {
-    Node current_node = queue.front();
-    add_neighbours_to_queue(current_node, map, reached, queue);
-    queue.pop();
-  }
-}
-
-void add_neighbours_to_queue(Node node,
-                             Map* map,
-                             std::vector<Node> reached,
-                             Queue queue) {}
--- src/path_finder.h
+++ src/path_finder.h
@@ -1,20 +1,15 @@
 #pragma once
 
 #include <queue>
-#include <vector>
+#include <unordered_set>
 #include "geometry.h"
 #include "map.h"
 
 struct Node {
   Position position;
   Node* previous;
-  Node* next;
 };
 
 typedef std::queue<Node> Queue;
 
-void BFS(Position entrance_pos, Position exit_pos, Map* map);
-void add_neighbours_to_queue(Node node,
-                             Map* map,
-                             std::vector<Node> reached,
-                             Queue queue);
+void BFS(void);
