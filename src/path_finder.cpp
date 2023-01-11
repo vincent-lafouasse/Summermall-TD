9fe521b1a120779cf4a27cda1902732d4a876714
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-11 17:34:24 +0100

    BFS sketch 2: Reloaded

--- src/path_finder.cpp
+++ src/path_finder.cpp
@@ -1,18 +1,35 @@
 #include "path_finder.h"
 
-void BFS(Position entrance) {
+void BFS(Position entrance, Map* map) {
   std::queue<Position> queue;
   std::map<Position, Position> reached;
 
   queue.push(entrance);
 
   while (!queue.empty()) {
-    Position root = queue.front();
-    add_neighbors_to_queue(root, queue, reached);
+    Position parent = queue.front();
+    add_neighbours_to_queue(parent, queue, reached, map);
     queue.pop();
   }
 }
 
-void add_neighbors_to_queue(Position node,
-                            std::queue<Position> queue,
-                            std::map<Position, Position> reached) {}
+void add_neighbours_to_queue(Position parent,
+                             std::queue<Position> queue,
+                             std::map<Position, Position> reached,
+                             Map* map) {
+  int n_neighbours;
+  Position* neighbours = get_neighbours(parent, map, &n_neighbours);
+
+  for (int i = 0; i < n_neighbours; i++) {
+    auto iterator = reached.find(neighbours[i]);
+    if (iterator != reached.end()) {
+      Position child = iterator->first;
+    }
+  }
+}
+
+Position* get_neighbours(Position node, Map* map, int* n_neighbours) {
+  Position* neighbours = {};
+  *n_neighbours = 0;
+  return neighbours;
+}
--- src/path_finder.h
+++ src/path_finder.h
@@ -5,7 +5,9 @@
 #include "geometry.h"
 #include "map.h"
 
-void BFS(Position entrance);
-void add_neighbors_to_queue(Position node,
-                            std::queue<Position> queue,
-                            std::map<Position, Position> reached);
+void BFS(Position entrance, Map* map);
+Position* get_neighbours(Position node, Map* map, int* n_neighbours);
+void add_neighbours_to_queue(Position node,
+                             std::queue<Position> queue,
+                             std::map<Position, Position> reached,
+                             Map* map);
