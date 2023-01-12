7efb85ae6a8d5082cd403954e3eac7082dd2ad4f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-12 17:22:29 +0100

    remove obsolete path-finder

--- src/path_finder.h
+++ src/path_finder.h
@@ -1,13 +1,2 @@
 #pragma once
-
-#include <map>
-#include <queue>
 #include "geometry.h"
-#include "map.h"
-
-void BFS(Position root, Map* map);
-Position* get_neighbours(Position node, Map* map, int* n_neighbours);
-void add_neighbours_to_queue(Position node,
-                             std::queue<Position> queue,
-                             std::map<Position, Position> reached,
-                             Map* map);
