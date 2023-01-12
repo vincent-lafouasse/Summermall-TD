d9c7393a1cc360b99f8938ed6a905ff6a5c7e877
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-12 17:31:15 +0100

    skeleton for pathfinding from hardcoded graph

--- src/path_finder.cpp
+++ src/path_finder.cpp
@@ -1,1 +1,7 @@
 #include "path_finder.h"
+
+Position* get_path(void) {
+  static std::map<Position, Position*> graph;
+}
+void add_edge(void) {}
+void add_vertex(void) {}
--- src/path_finder.h
+++ src/path_finder.h
@@ -1,2 +1,8 @@
 #pragma once
+#include <map>
 #include "geometry.h"
+
+std::map<Position, Position*> make_graph(void);
+Position* get_path(void);
+void add_edge(void);
+void add_vertex(void);
