33990506fa0f70dc39d07bbff335d57e00f85a88
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-12 17:38:46 +0100

    refactor

--- src/path_finder.h
+++ src/path_finder.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <map>
+#include <vector>
 #include "geometry.h"
 
-std::map<Position, Position*> make_graph(void);
+std::map<Position, std::vector<Position>> make_graph(void);
 Position* get_path(void);
 void add_edge(void);
 void add_vertex(void);
