7220036eb409f62358ba5ffbf28f6de6fe9fb0ce
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-12 16:06:39 +0100

    minor refactor

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -28,9 +28,9 @@ void Monster::render(SDL_Renderer* renderer) {
 
 void Monster::move_to(Position destination) {
   int x_movement =
-      (m_position.x != destination.x) * (m_position.x > destination.x ? -1 : 1);
+      (m_position.x != destination.x) * (m_position.x < destination.x ? 1 : -1);
   int y_movement =
-      (m_position.y != destination.y) * (m_position.y > destination.y ? -1 : 1);
+      (m_position.y != destination.y) * (m_position.y < destination.y ? 1 : -1);
   move_by(x_movement, y_movement);
 }
 
