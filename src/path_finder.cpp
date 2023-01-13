40bf845c68a71a5ee0e43e8cc6e52615713ceb26
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-13 20:31:09 +0100

    partial implementation of bresenham (good enough for now)

--- src/path_finder.cpp
+++ src/path_finder.cpp
@@ -1,5 +1,20 @@
 #include "path_finder.h"
 
 std::vector<Position> get_Bresenham_line_between(Position start, Position end) {
+  int delta_x = end.x - start.x;
+  int delta_y = end.y - start.y;
+  int difference = 2 * delta_y - delta_x;
+  int y = start.y;
 
+  std::vector<Position> line;
+
+  for (int x = start.x; x <= end.x; x++) {
+    line.push_back({x, y});
+    if (difference > 0) {
+      y++;
+      difference -= 2 * delta_x;
+    }
+    difference += 2 * delta_y;
+  }
+  return line;
 }
