e8c14b53e5ad0057925e3e62ad50868a91399c25
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-13 22:47:37 +0100

    minor refactor

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -15,34 +15,12 @@ Monster::Monster(Position position,
   m_edge_id = 0;
 }
 
-void Monster::move_by(int delta_x, int delta_y) {
-  m_position.x += delta_x;
-  m_position.y += delta_y;
-}
-
-void Monster::render(SDL_Renderer* renderer) {
-  Position centered_position = {
-      m_position.x - (m_shape.w / 2),
-      m_position.y - (m_shape.h / 2),
-  };
-  SDL_Rect dst_tile_loc = {centered_position.x, centered_position.y, m_shape.w,
-                           m_shape.h};
-  SDL_RenderCopyEx(renderer, m_texture, NULL, &dst_tile_loc, m_orientation,
-                   NULL, SDL_FLIP_NONE);
-}
-
 int sign_of(float number) {
   return (number != 0) * (number > 0 ? 1 : -1);
 }
 
 int sign_of(int number) {
   return (number != 0) * (number > 0 ? 1 : -1);
-}
-
-void Monster::step_toward_simple(Position destination) {
-  int delta_x = destination.x - m_position.x;
-  int delta_y = destination.y - m_position.y;
-  move_by(sign_of(delta_x), sign_of(delta_y));
 }
 
 void Monster::follow_path(std::vector<Position> path) {
@@ -57,4 +35,26 @@ void Monster::follow_line(std::vector<Position> line) {
   step_toward_simple(line[m_step_id]);
   if (m_position == line[m_step_id])
     m_step_id++;
+}
+
+void Monster::step_toward_simple(Position destination) {
+  int delta_x = destination.x - m_position.x;
+  int delta_y = destination.y - m_position.y;
+  move_by(sign_of(delta_x), sign_of(delta_y));
+}
+
+void Monster::move_by(int delta_x, int delta_y) {
+  m_position.x += delta_x;
+  m_position.y += delta_y;
+}
+
+void Monster::render(SDL_Renderer* renderer) {
+  Position centered_position = {
+      m_position.x - (m_shape.w / 2),
+      m_position.y - (m_shape.h / 2),
+  };
+  SDL_Rect dst_tile_loc = {centered_position.x, centered_position.y, m_shape.w,
+                           m_shape.h};
+  SDL_RenderCopyEx(renderer, m_texture, NULL, &dst_tile_loc, m_orientation,
+                   NULL, SDL_FLIP_NONE);
 }
