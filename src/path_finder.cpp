789dd7b0e0c24b11dc7f2048414fc4a548f98b89
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-13 22:35:57 +0100

    implem full Bresenham

--- src/path_finder.cpp
+++ src/path_finder.cpp
@@ -25,6 +25,30 @@ static std::vector<Position> Bresenham_small_delta_y(Position start,
   return line;
 }
 
+static std::vector<Position> Bresenham_small_delta_x(Position start,
+                                                     Position end) {
+  int delta_y = end.y - start.y;
+  int delta_x = end.x - start.x;
+  int x_increment = 1;
+  if (delta_x < 0) {
+    x_increment = -1;
+    delta_x = -delta_x;
+  }
+  int difference = 2 * delta_x - delta_y;
+  int x = start.x;
+  std::vector<Position> line;
+
+  for (int y = start.y; y <= end.y; y++) {
+    line.push_back({x, y});
+    if (difference > 0) {
+      x += x_increment;
+      difference -= 2 * delta_y;
+    }
+    difference += 2 * delta_x;
+  }
+  return line;
+}
+
 std::vector<Position> get_Bresenham_line_between(Position start,
                                                  Position end,
                                                  float* return_orientation) {
@@ -33,13 +57,19 @@ std::vector<Position> get_Bresenham_line_between(Position start,
   std::vector<Position> line;
 
   if (abs(delta_y) < abs(delta_x)) {
-    if (delta_x > 0) {
+    if (delta_x >= 0) {
       line = Bresenham_small_delta_y(start, end);
     } else {
       line = Bresenham_small_delta_y(end, start);
       std::reverse(line.begin(), line.end());
     }
   } else {
+    if (delta_y >= 0) {
+      line = Bresenham_small_delta_x(start, end);
+    } else {
+      line = Bresenham_small_delta_x(end, start);
+      std::reverse(line.begin(), line.end());
+		}
   }
 
   *return_orientation = atan((float)delta_y / (float)delta_x);
