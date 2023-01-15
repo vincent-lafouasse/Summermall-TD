9da4911bdb3f84cce63c2f886b09ae3da43a0266
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-15 15:38:17 +0100

    succinct docstring for the `geometry` module

--- src/geometry.h
+++ src/geometry.h
@@ -19,14 +19,18 @@ struct Position {
 };
 
 // Return the 2D coordinates corresponding to the 1D coodinate in a rectangle of
-// given width.
+// a given width.
 //
 // @param pos_1D The 1D coordinate to convert.
-// @param width The width of the rectangle in which the 1D coordinate is.
+// @param width  The width of the rectangle in which the 1D coordinate is.
 //
-// @return A Rectangle object containing the width and height of `texture`.
+// @return A Position object containing the 2D coordinates.
 Position pos_1D_to_2D(int pos_1D, int width);
 int pos_2D_to_1D(Position position, int width);
 Position pixel_pos_from_grid(Position grid_pos, Dimension tileshape);
+
+// Return the Euclidian distance between two Positions.
 float distance(Position a, Position b);
+
+// Return the rasterized line between two Positions.
 std::vector<Position> get_Bresenham_line_between(Position start, Position end);
