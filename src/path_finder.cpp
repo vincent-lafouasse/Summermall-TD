07b65cf999b82f68c90f96ecfda65884dc5cdf7b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-22 16:07:17 +0100

    move graph methods from header to cpp file

--- src/path_finder.cpp
+++ src/path_finder.cpp
@@ -1,1 +1,26 @@
 #include "path_finder.h"
+
+void Graph::add_vertex(Position position) {
+  if (adjacency_map.find(position) != adjacency_map.end()) {
+    std::set<Position> empty;
+    adjacency_map[position] = empty;
+  }
+}
+
+void Graph::add_vertices(std::vector<Position>* positions) {
+  for (size_t i = 0; i < positions->size(); i++) {
+    add_vertex(positions->at(i));
+  }
+}
+
+void Graph::add_edge(Position key, Position value) {
+  if (value != key) {
+    adjacency_map[key].insert(value);
+  }
+}
+
+void Graph::add_edges(Position key, std::vector<Position>* values) {
+  for (size_t i = 0; i < values->size(); i++) {
+    add_edge(key, values->at(i));
+  }
+}
--- src/path_finder.h
+++ src/path_finder.h
@@ -7,28 +7,8 @@
 struct Graph {
   std::map<Position, std::set<Position>> adjacency_map;
 
-  void add_vertex(Position position) {
-    if (adjacency_map.find(position) != adjacency_map.end()) {
-      std::set<Position> empty;
-      adjacency_map[position] = empty;
-    }
-  }
-
-  void add_vertices(std::vector<Position>* positions) {
-    for (size_t i = 0; i < positions->size(); i++) {
-      add_vertex(positions->at(i));
-    }
-  }
-
-  void add_edge(Position key, Position value) {
-    if (value != key) {
-      adjacency_map[key].insert(value);
-    }
-  }
-
-  void add_edges(Position key, std::vector<Position>* values) {
-    for (size_t i = 0; i < values->size(); i++) {
-      add_edge(key, values->at(i));
-    }
-  }
+  void add_vertex(Position position);
+  void add_vertices(std::vector<Position>* positions);
+  void add_edge(Position key, Position value);
+  void add_edges(Position key, std::vector<Position>* values);
 };
