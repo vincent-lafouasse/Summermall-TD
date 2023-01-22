89317c3a16ac5f524784d367e0825535c4eb9384
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-01-22 15:24:28 +0100

    clear path-finder

--- src/path_finder.h
+++ src/path_finder.h
@@ -1,32 +1,4 @@
 #pragma once
-#include <queue>
+#include <map>
 #include <vector>
 #include "geometry.h"
-
-#define DISTANCE_PRECISION 1000
-
-struct Edge {
-  Position start;
-  Position end;
-  int distance;
-
-  Edge(Position start_, Position end_) {
-    start = start_;
-    end = end_;
-    distance = (int)(DISTANCE_PRECISION * euclidian_distance(start, end));
-  }
-  bool contains(Position position);
-};
-
-struct Graph {
-  std::vector<Position> vertices;
-  std::vector<Edge> edges;
-
-  void add_vertex(Position position);
-  void add_edge(Position end, Position start);
-  std::vector<Position> neighbours_of(Position node);
-};
-
-std::vector<Position> compute_path_between(Graph graph,
-                                           Position start,
-                                           Position end);
